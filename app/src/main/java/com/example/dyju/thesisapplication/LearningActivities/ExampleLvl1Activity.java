3a5aa5d52468465e64064f9da3601ba2a9cfe3b8
dyju1992 <dyju1992@gmail.com>
2017-03-15 22:53:03 +0100

    adding activities in learning lvl1

---
+++ app/src/main/java/com/example/dyju/thesisapplication/LearningActivities/ExampleLvl1Activity.java
@@ -0,0 +1,7 @@
+package com.example.dyju.thesisapplication.LearningActivities;
+
+/**
+ * Created by dyju on 2017-03-15.
+ */
+public class ExampleLvl1Activity {
+}
---
+++ app/src/main/java/com/example/dyju/thesisapplication/LearningActivities/ForWhoLvl1Activity.java
@@ -0,0 +1,9 @@
+package com.example.dyju.thesisapplication.LearningActivities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dyju on 2017-03-15.
+ */
+public class ForWhoLvl1Activity extends AppCompatActivity {
+}
