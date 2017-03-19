96402abd0e1c82824654a7c322dbef41e2e5213a
dyju1992 <dyju1992@gmail.com>
2017-03-19 21:29:42 +0100

    Adding action bar in AboutManipulatorActivity

---
+++ app/src/main/java/com/example/dyju/thesisapplication/LearningActivities/AboutManipulatorBars/ManipulatorDefinition.java
@@ -0,0 +1,7 @@
+package com.example.dyju.thesisapplication.LearningActivities.AboutManipulatorBars;
+
+/**
+ * Created by dyju on 2017-03-19.
+ */
+public class ManipulatorDefinition {
+}
---
+++ app/src/main/java/com/example/dyju/thesisapplication/LearningActivities/AboutManipulatorBars/ManipulatorPurposes.java
@@ -0,0 +1,25 @@
+package com.example.dyju.thesisapplication.LearningActivities.AboutManipulatorBars;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dyju.thesisapplication.R;
+
+/**
+ * Created by dyju on 2017-03-19.
+ */
+public class ManipulatorPurposes extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                 Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.manipulator_definition, container, false);
+
+        return view;
+
+    }
+}
